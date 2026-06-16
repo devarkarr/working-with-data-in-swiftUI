481548a34a64f862b570b48b43e96380a752f3d7
arkarlin2003 <arkarlin486@gmail.com>
2026-06-16 10:32:48 +0630

    @State

--- working with data/Views/DogsView.swift
+++ working with data/Views/DogsView.swift
@@ -6,20 +6,38 @@
 //
 import SwiftUI
 
+struct NameData {
+    var firstName = ""
+    var lastName = ""
+}
+
 struct DogsView: View {
     @State private var oo = DogsOO()
+    @State  var nameInfo = NameData()
     var body: some View {
-        List(oo.data) { datum in
-            Text(datum.name)
-        }
-        .font(.title)
-        .onAppear {
-            oo.fetch()
+        VStack {
+            
+            VStack {
+                TextField("First Name", text: $nameInfo.firstName)
+                TextField("Last Name", text: $nameInfo.lastName)
+            }
+            .padding()
+          
+            List(oo.data) { datum in
+                Text(datum.name)
+            }
+            .font(.title)
+            .onAppear {
+                oo.fetch()
+            }
+           
+            
+
         }
     }
 }
 
 
 #Preview {
-    DogsView()
+    DogsView(nameInfo: NameData(firstName: "arkar",lastName: "lin"))
 }
