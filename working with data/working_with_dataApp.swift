55a4b5f969aea28e692840d86430d471e0a7f897
arkarlin2003 <arkarlin486@gmail.com>
2026-06-16 09:33:36 +0630

    Initial Commit

---
+++ working with data/working_with_dataApp.swift
@@ -0,0 +1,17 @@
+//
+//  working_with_dataApp.swift
+//  working with data
+//
+//  Created by Ar Kar Lin on 6/16/26.
+//
+
+import SwiftUI
+
+@main
+struct working_with_dataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
